2c107579b5b8fdca8e4099aa97fbc9dbf940c92d
IlyaVishnikin <rarebloor@gmail.com>
2023-03-25 11:49:37 +0300

    inifite loop

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ $(function() {
 
   $('.product-slider').slick({
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 1500,
     arrows: false,
     dots: true,
-    fade: true
+    fade: true,
   });
 
   $('.buy-slider').slick({
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 2000,
     arrows: false,
     dots: true,
